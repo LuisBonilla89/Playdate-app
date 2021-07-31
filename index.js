55a9c13fc401c3520605ea1cb945c968cdc85de4
Luis Bonilla <luis.bonilla8907@outlook.com>
2021-07-31 23:49:24 +1000

    commit

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 const { ApolloServer } = require("apollo-server");
 
-const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config.js");
-const typeDefs = gql`
-  type Query {
-    sayHi: String!
-  }
-`;
+
 //Adding resolvers
-const resolvers = {
-  Query: {
-    sayHi: () => "Hello World!!!!!",
-  },
-};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req }) => ({ req }),
 });
 
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log("MongoDB connected");
+    console.log("MongoDB connection is successful!!!");
     return server.listen({ port: 5000 });
   })
   .then((res) => {
