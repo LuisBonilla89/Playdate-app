a98f7e5c1c6ff01c04c7797913a2cd1ce5c787db
Luis Bonilla <luis.bonilla8907@outlook.com>
2021-08-01 16:03:05 +1000

    adding validation statement for body comments

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
@@ -29,7 +29,9 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      console.log(user);
+      if (argsToArgsConfig.body.trim() === "") {
+        throw new Error("Post body cannot be empty");
+      }
 
       const newPost = new Post({
         body,
@@ -55,6 +57,28 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        //making sure that the post actually exist for it to be given a like
+
+        if (post.likes.find((like) => like.username === username)) {
+          //Post at this stage is already being like, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          //Notliked, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
     },
   },
 };
