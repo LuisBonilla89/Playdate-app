2e593c88ee6f796a2974bb641b53a391b6649d28
LuisBonilla89 <78412087+LuisBonilla89@users.noreply.github.com>
2021-08-10 22:40:49 +1000

    heroku

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://Luis:Kike890712@@cluster0.fgntw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  SECRET_KEY: "<random_secret_key_here>",
+};
