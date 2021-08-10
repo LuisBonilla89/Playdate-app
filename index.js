0727a28d7875955283c1ed7d6b155cb69c03ad36
LuisBonilla89 <78412087+LuisBonilla89@users.noreply.github.com>
2021-08-10 22:31:24 +1000

    fixing dependencies

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // Dependencies
-const { ApolloServer, PubSub } = require("apollo-server");
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { PubSub } = require("apollo-server");
 const mongoose = require("mongoose");
+const { MONGODB } = require("./config");
 
 // Relatives
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
-const { MONGODB } = require("./config");
+const { db } = require("./models/Post");
+
+const app = express();
 
 const pubSub = new PubSub();
 
@@ -17,6 +22,25 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req, pubSub }),
 });
 
+server.applyMiddleware({ app });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// db.once("open", () => {
+//   app.listen(PORT, () => {
+//     console.log(`API running on port ${PORT}!`);
+//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//   });
+// });
+
 mongoose
   .connect(process.env.MONGODB_URI || MONGODB, {
     useNewUrlParser: true,
@@ -26,11 +50,7 @@ mongoose
   })
   .then(() => {
     console.log("😃 You are connected to MongoDB");
-    return server.listen({ port: PORT });
+    return app.listen({ port: PORT });
   })
-  .then((res) => console.log(` 🚀🚀🚀 The server is  running at ${res.url}`))
+  .then((res) => console.log(` 🚀🚀🚀 The server is  running at ${PORT}`))
   .catch((err) => console.log(`Error Starting Server => ${err}`));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
