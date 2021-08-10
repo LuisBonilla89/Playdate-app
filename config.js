802e92f658bd00d6eaa445f034b58d195c5615f1
LuisBonilla89 <78412087+LuisBonilla89@users.noreply.github.com>
2021-08-10 23:33:14 +1000

    environment variables

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGODB:
-    "mongodb+srv://Luis:Kike890712@@cluster0.fgntw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  MONGODB: process.env.MONGO_DB,
+
   SECRET_KEY: "<random_secret_key_here>",
 };
